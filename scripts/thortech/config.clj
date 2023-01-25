c89774ec8dfc0a7e60fa0d3b77adcf015c277246
Tom Hickey <thickey@gmail.com>
2023-01-25 15:17:51 -0500

    renamed invoke to get-token. added get-value. support multiple tokens

--- scripts/thortech/config.clj
+++ scripts/thortech/config.clj
@@ -1,6 +1,7 @@
 (ns thortech.config
   (:require [cheshire.core :as json]
             [clojure.java.io :as io]
+            [clojure.string :as str]
             [clojure.tools.cli :refer [parse-opts]]
             [com.grzm.awyeah.client.api :as aws]))
 
@@ -11,25 +12,41 @@
     ;; :parse-fn #(Integer/parseInt %)
     ;; :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]
     ]
-   ["-t" "--token TOKEN" "Token name, e.g. LegacyConfig"
-    :default "LegacyConfig"]
+   ["-t" "--tokens TOKEN" "Token name, e.g. LegacyConfig"
+    :multi true
+    :default []
+    :update-fn conj]
+   ["-k" "--key KEY" "Key name, e.g. DatabaseName"]
    ["-h" "--help"]])
 
-(defn invoke-lambda [client env token]
-  (let [payload (io/input-stream (.getBytes (json/encode {:requestType "Query"
-                                                          :compressResults false
-                                                          :environmentName env
-                                                          :tokenNames [token]})))
-        out (aws/invoke client {:op :Invoke
-                                :request
-                                {:FunctionName "ConfigurationKeyRequests"
-                                 :Payload payload}})]
-    (if (:Payload out)
-      (with-open [r (io/reader (:Payload out))] 
-        (doall (-> r (json/parse-stream true) :Values)))
-      (do 
-        (println out)
-        (throw (ex-info "No :Paylod found in output from lambda invoke." {:data out}))))))
+(defn get-token
+  ([env tokens]
+   (get-token (aws/client {:api :lambda}) env tokens))
+  ([client env tokens]
+   (let [params (json/encode {:requestType "Query"
+                              :compressResults false
+                              :environmentName env
+                              :tokenNames (into [] tokens)})
+         payload (io/input-stream (.getBytes params))
+         out (aws/invoke client {:op :Invoke
+                                 :request
+                                 {:FunctionName "ConfigurationKeyRequests"
+                                  :Payload payload}})]
+     (if (:Payload out)
+       (with-open [r (io/reader (:Payload out))] 
+         (doall (-> r (json/parse-stream true) :Values)))
+       (do 
+         (println out)
+         (throw (ex-info "No :Paylod found in output from lambda invoke." {:data out})))))))
+
+(defn get-value
+  ([env token key]
+   (get-value (aws/client {:api :lambda}) env token key))
+  ([client env token key]
+   (let [token-data (get-token client env token)
+         key (str/lower-case key)]
+     (some #(when (= (str/lower-case (:KeyName %)) key)
+              (:Value %)) token-data))))
 
 (defn exit
   "Helper to wrap System/exit"
@@ -44,7 +61,9 @@
           (exit 0 summary)
 
           (empty? errors)
-          (let [client (aws/client {:api :lambda})]
-            (invoke-lambda client
-                           (:env options)
-                           (:token options))))))
+          (if (:key options)
+            (get-value (:env options)
+                       (:tokens options)
+                       (:key options))
+            (get-token (:env options)
+                       (:tokens options))))))
